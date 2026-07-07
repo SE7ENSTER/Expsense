76e7153d1381ef6aeea8c6ce86ffa40f1d628d00
SE7ENSTER <love.nona@gmail.com>
2026-07-07 14:40:14 +0700

    Initialize budget approval extension

--- docs/budget-approval.js
+++ docs/budget-approval.js
@@ -1,1 +1,6 @@
-console.log('ExpenseFlow budget approval extension loaded');
+import { getApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
+const auth=getAuth(getApp());
+const db=getFirestore(getApp());
+console.log('ExpenseFlow budget approval extension loaded', Boolean(auth), Boolean(db));
